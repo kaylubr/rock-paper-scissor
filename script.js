57a27a5b5ae0a7500f45b43ed94c470ae33a8514
kaylubr <kylebreyes092005@gmail.com>
2024-07-18 12:47:30 +0800

    Add function getHumanChoice
    This function takes user input choosing rock, paper, or scissors

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ function getComputerChoice() {
     choice = Math.floor(Math.random() * 3);
 
     if (choice == 1) {
-        return "Rock";
+        return "ROCK";
     } else if (choice == 2) {
         return "Paper";
     } else {
         return "Scissor";
     }
 }
+
+function getHumanChoice() {
+    choice = prompt("Choice: ");
+
+    return choice.toUpperCase();
+}
+
+console.log(getHumanChoice());
