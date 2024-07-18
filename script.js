69fe86ac59bf36eb780caf21edf438349ed9dff5
kaylubr <kylebreyes092005@gmail.com>
2024-07-18 13:05:22 +0800

    Add playRound function
    This function takes 2 arguments for comparing two choices and adding score to whoever wins

--- script.js
+++ script.js
@@ -5,16 +5,44 @@ function getComputerChoice() {
     choice = Math.floor(Math.random() * 3);
 
     if (choice == 1) {
-        return "ROCK";
+        return "rock";
     } else if (choice == 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissor";
+        return "scissor";
     }
 }
 
 function getHumanChoice() {
     choice = prompt("Choice: ");
 
-    return choice.toUpperCase();
+    return choice.toLowerCase();
+}
+
+// console.log(`You win! ${humanChoice} beats $          {computerChoice}`);
+
+// console.log(`You lose! ${computerChoice} beats ${computerChoice}.`);
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissor") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if (humanChoice === "scissor" && computerChoice === "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if (humanChoice === "scissor" && computerChoice === "rock") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissor") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    } else {
+        console.log("Tie!");
+    }
 }
