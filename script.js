16ff687592e0afe1cd41125b8449d1368986221c
kaylubr <kylebreyes092005@gmail.com>
2024-07-24 13:08:14 +0800

    Add images references to script.js

--- script.js
+++ script.js
@@ -3,13 +3,42 @@ let computerScoreCount = 0;
 const humanScore = document.querySelector('#player-one-score');
 const computerScore = document.querySelector('#player-ai-score');
 
+//reference for the choices 
 const humanChoice = document.querySelector(".choices-container");
 const computerChoice = ['rock', 'paper', 'scissor'];    
 
 const resetBtn = document.querySelector('#reset');
+
+//reference for the result status 
 const result = document.querySelector("#result");
 
+//reference for the two big output containers
+const playerOutput = document.querySelector(".playerOne");
+const computerOutput = document.querySelector(".playerAI");
+
+//reference for the images
+const rockImage = document.querySelector("#rock");
+const paperImage = document.querySelector("#paper");
+const scissorImage = document.querySelector("#scissor");
+const winnerImage = document.createElement("img");
+winnerImage.src = "./img/happy-1-svgrepo-com.svg";
+winnerImage.classList.add("image-clone");
+const loserImage = document.createElement("img");
+loserImage.src = "./img/cry-svgrepo-com.svg";
+loserImage.classList.add("image-clone");
+
+//cloning the images
+const rockImageClone = rockImage.cloneNode(true);
+const paperImageClone = paperImage.cloneNode(true);
+const scissorImageClone = scissorImage.cloneNode(true);
+rockImageClone.classList.add("image-clone");
+paperImageClone.classList.add("image-clone");
+scissorImageClone.classList.add("image-clone");
+
+//event handler for reset button
 resetBtn.addEventListener('click', () => {
+    playerOutput.innerHTML = "";
+    computerOutput.innerHTML = "";
     humanScoreCount = 0;
     computerScoreCount = 0;
     result.textContent = "";
@@ -36,54 +65,120 @@ humanChoice.addEventListener("click", (event) => {
         humanScoreCount = 0;
         computerScoreCount = 0;
         result.textContent = "You won the game!";
+
+        //showing the output for the winner or loser
         humanScore.textContent = `You: ${humanScoreCount}`;
         computerScore.textContent = `Computer: ${computerScoreCount}`;
+
+        playerOutput.innerHTML = "";
+        computerOutput.innerHTML = "";
+        playerOutput.appendChild(winnerImage);
+        computerOutput.appendChild(loserImage);
     } else if (computerScoreCount === 5) {
         console.log("You lose");
         humanScoreCount = 0;
         computerScoreCount = 0;
         result.textContent = "You lost the game!";
+
+        //showing the output for the winner or loser
         humanScore.textContent = `You: ${humanScoreCount}`;
         computerScore.textContent = `Computer: ${computerScoreCount}`;
+
+        playerOutput.innerHTML = "";
+        computerOutput.innerHTML = "";
+        playerOutput.appendChild(loserImage);
+        computerOutput.appendChild(winnerImage);
     }
 });
 
 
 function playRound(human, computer) {
+    playerOutput.innerHTML = "";
+    computerOutput.innerHTML = "";
+
     if (human == "rock" && computer === "scissor") {
         console.log(`You win! ${human} beats ${computer}`);
+
         humanScoreCount++;
         result.textContent = `You win! ${human} beats ${computer}`;
         humanScore.textContent = `You: ${humanScoreCount}`;
-    } else if (human == "paper" && computer === "rock") {
+
+        playerOutput.appendChild(rockImageClone);
+        computerOutput.appendChild(scissorImageClone);
+    } 
+    
+    else if (human == "paper" && computer === "rock") {
+        console.log(`You win! ${human} beats ${computer}`);
+
+        humanScoreCount++;
+        result.textContent = `You win! ${human} beats ${computer}`;
+        humanScore.textContent = `You: ${humanScoreCount}`;
+
+        playerOutput.appendChild(paperImageClone);
+        computerOutput.appendChild(rockImageClone);
+    } 
+    
+    else if (human == "scissor" && computer === "paper") {
         console.log(`You win! ${human} beats ${computer}`);
         humanScoreCount++;
         result.textContent = `You win! ${human} beats ${computer}`;
         humanScore.textContent = `You: ${humanScoreCount}`;
-    } else if (human == "scissor" && computer === "paper") {
-        console.log(`You win! ${human} beats ${computer}`);
-        humanScoreCount++;
-        result.textContent = `You win! ${human} beats ${computer}`;
-        humanScore.textContent = `You: ${humanScoreCount}`;
-    } else if (human == "scissor" && computer === "rock") {
+
+        playerOutput.appendChild(scissorImageClone);
+        computerOutput.appendChild(paperImageClone);
+    } 
+    
+    else if (human == "scissor" && computer === "rock") {
         console.log(`You lose! ${computer} beats ${human}`);
+
         computerScoreCount++;
         result.textContent = `You lose! ${computer} beats ${human}`;
         computerScore.textContent = `Computer: ${computerScoreCount}`;
-    } else if (human == "rock" && computer === "paper") {
+
+        playerOutput.appendChild(scissorImageClone);
+        computerOutput.appendChild(rockImageClone);
+    } 
+    
+    else if (human == "rock" && computer === "paper") {
         console.log(`You lose! ${computer} beats ${human}`);
+
         computerScoreCount++;
         result.textContent = `You lose! ${computer} beats ${human}`;
         computerScore.textContent = `Computer: ${computerScoreCount}`;
-    } else if (human == "paper" && computer === "scissor") {
+
+        playerOutput.appendChild(rockImageClone);
+        computerOutput.appendChild(paperImageClone);
+    } 
+    
+    else if (human == "paper" && computer === "scissor") {
         console.log(`You lose! ${computer} beats ${human}`);
+
         computerScoreCount++;
         result.textContent = `You lose! ${computer} beats ${human}`;
         computerScore.textContent = `Computer: ${computerScoreCount}`;
-    } else {
+
+        playerOutput.appendChild(paperImageClone);
+        computerOutput.appendChild(scissorImageClone);
+    } 
+    
+    else {
         console.log(`Tie! ${human} is the same with ${computer}`);
         result.textContent = `Tie! ${human} is the same with ${computer}`;
 
+        switch(human) {
+            case 'rock':
+                playerOutput.appendChild(rockImageClone);
+                computerOutput.appendChild(rockImageClone.cloneNode(true));
+                break;
+            case 'paper':
+                playerOutput.appendChild(paperImageClone);
+                computerOutput.appendChild(paperImageClone.cloneNode(true));
+                break;
+            case 'scissor':
+                playerOutput.appendChild(scissorImageClone);
+                computerOutput.appendChild(scissorImageClone.cloneNode(true));
+                break;    
+        }
     }
 }
 
